2dd2a1b652b54207e633a083df89de5f6d027bf6
ChuckBTaylor <WLScripps@gmail.com>
2017-09-18 21:19:23 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  @@all = []
+  attr_reader :name,:weight, :height
+
+  def initialize(pirate_hash = {})
+    @name = pirate_hash[:name]
+    @weight = pirate_hash[:weight]
+    @height = pirate_hash[:height]
+    @@all << self
+  end
+
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  @@all = []
+  attr_reader :name, :type, :booty
+
+  def initialize(ship_hash = {})
+    @name = ship_hash[:name]
+    @type = ship_hash[:type]
+    @booty = ship_hash[:booty]
+    @@all << self
+  end
+
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
 end
